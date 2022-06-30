7ec37e27cdb1c68592739853a82aedf14d9f5d7b
junstyle <junstyle@git>
2022-06-30 16:35:50 +0800

    remove useless code and package config

--- src/extension.js
+++ src/extension.js
@@ -5,8 +5,6 @@ import twigArr from "./hover/twig.json";
 import { formatting } from "./formatting";
 import { clearVirtualDocumentContents, createVirtualDoc, registerTextDocumentEvents } from "./virtualDocument";
 
-const config = vscode.workspace.getConfiguration("twig-language");
-
 function createHover(snippet, type) {
   const example = typeof snippet.example == "undefined" ? "" : snippet.example;
   const description = typeof snippet.description == "undefined" ? "" : snippet.description;
@@ -27,76 +25,72 @@ function activate(context) {
   context.subscriptions.push(diagnosticCollection);
 
   function registerDocType(type) {
-    if (config.hover === true) {
-      context.subscriptions.push(
-        vscode.languages.registerHoverProvider(type, {
-          async provideHover(document, position) {
-            const range = document.getWordRangeAtPosition(position);
-            const word = document.getText(range);
+    context.subscriptions.push(
+      vscode.languages.registerHoverProvider(type, {
+        async provideHover(document, position) {
+          const range = document.getWordRangeAtPosition(position);
+          const word = document.getText(range);
 
-            for (const snippet in snippetsArr) {
-              if (snippetsArr[snippet].prefix == word || snippetsArr[snippet].hover == word) {
-                return createHover(snippetsArr[snippet], type);
-              }
+          for (const snippet in snippetsArr) {
+            if (snippetsArr[snippet].prefix == word || snippetsArr[snippet].hover == word) {
+              return createHover(snippetsArr[snippet], type);
             }
+          }
 
-            for (const snippet in functionsArr) {
-              if (functionsArr[snippet].prefix == word || functionsArr[snippet].hover == word) {
-                return createHover(functionsArr[snippet], type);
-              }
+          for (const snippet in functionsArr) {
+            if (functionsArr[snippet].prefix == word || functionsArr[snippet].hover == word) {
+              return createHover(functionsArr[snippet], type);
             }
+          }
 
-            for (const snippet in twigArr) {
-              if (twigArr[snippet].prefix == word || twigArr[snippet].hover == word) {
-                return createHover(twigArr[snippet], type);
-              }
+          for (const snippet in twigArr) {
+            if (twigArr[snippet].prefix == word || twigArr[snippet].hover == word) {
+              return createHover(twigArr[snippet], type);
             }
+          }
 
+          const vdocUri = createVirtualDoc(document);
+          const hs = await commands.executeCommand("vscode.executeHoverProvider", vdocUri, position);
+          return hs?.[0];
+        }
+      })
+    );
+
+    context.subscriptions.push(
+      vscode.languages.registerDocumentFormattingEditProvider(type, {
+        provideDocumentFormattingEdits: async (document, options, token) => {
+          const otext = document.getText();
+          if (!otext) {
+            return;
+          }
+          let newDoc = document;
+
+          const text = formatting(newDoc, diagnosticCollection);
+          if (text && text != otext) {
+            const range = new Range(document.positionAt(0), document.positionAt(otext.length));
+            return [new TextEdit(range, text)];
+          } else {
+            return [];
+          }
+        }
+      })
+    );
+
+    context.subscriptions.push(
+      languages.registerCompletionItemProvider(
+        type,
+        {
+          provideCompletionItems: async (document, position, token, context) => {
             const vdocUri = createVirtualDoc(document);
-            const hs = await commands.executeCommand("vscode.executeHoverProvider", vdocUri, position);
-            return hs?.[0];
+            return await commands.executeCommand("vscode.executeCompletionItemProvider", vdocUri, position, context.triggerCharacter);
           }
-        })
-      );
-    }
-
-    if (config.formatting === true) {
-      context.subscriptions.push(
-        vscode.languages.registerDocumentFormattingEditProvider(type, {
-          provideDocumentFormattingEdits: async (document, options, token) => {
-            const otext = document.getText();
-            if (!otext) {
-              return;
-            }
-            let newDoc = document;
-
-            const text = formatting(newDoc, diagnosticCollection);
-            if (text && text != otext) {
-              const range = new Range(document.positionAt(0), document.positionAt(otext.length));
-              return [new TextEdit(range, text)];
-            } else {
-              return [];
-            }
-          }
-        })
-      );
-
-      context.subscriptions.push(
-        languages.registerCompletionItemProvider(
-          type,
-          {
-            provideCompletionItems: async (document, position, token, context) => {
-              const vdocUri = createVirtualDoc(document);
-              return await commands.executeCommand("vscode.executeCompletionItemProvider", vdocUri, position, context.triggerCharacter);
-            }
-          },
-          ".",
-          "(",
-          ":",
-          "<"
-        )
-      );
-    }
+        },
+        ".",
+        "(",
+        ":",
+        "<"
+      )
+    );
   }
 }
 
